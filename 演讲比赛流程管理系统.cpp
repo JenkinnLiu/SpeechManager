9c23f65a63b0f3b2b3bb91a2383f90acacd62e1e
JenkinnLiu <1419359341@qq.com>
2024-06-22 15:19:39 +0800

    Upadate Readme.md

--- 演讲比赛流程管理系统.cpp
+++ 演讲比赛流程管理系统.cpp
@@ -4,11 +4,6 @@ using namespace std;
 
 int main() {
 	SpeechManager sm;
-	/*for (map<int, Speaker>::iterator it = sm.m_Speaker.begin(); it != sm.m_Speaker.end(); it++) {
-		cout << "选手编号：" << it->first
-			<< " 姓名：" << it->second.m_Name
-			<< " 成绩：" << it->second.m_Score[0] << endl;
-	}*/
 	int choice = 0;
 	while (true) {
 		sm.Show_Menu();
